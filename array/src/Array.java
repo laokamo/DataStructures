6b9447bfda85aaca21e4683167647fd7ac10b64b
luo hao <laokamo@gmail.com>
2019-11-07 18:53:18 +0800

    feat(数组): 数组功能

--- array/src/Array.java
+++ array/src/Array.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @Description
  * @Author luo hao
@@ -12,6 +14,7 @@ public class Array {
 
     /**
      * 自定义初始化
+     *
      * @param capacity
      */
     public Array(int capacity) {
@@ -28,26 +31,192 @@ public class Array {
 
     /**
      * 判断是否为空
+     *
      * @return
      */
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
     /**
      * 元素个数
+     *
      * @return
      */
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
     /**
      * 数组的容量
+     *
      * @return
      */
-    public int getCapacity(){
+    public int getCapacity() {
         return data.length;
     }
 
+    /**
+     * 尾部元素
+     *
+     * @param element
+     * @return
+     */
+    public void addLast(int element) {
+        add(size, element);
+    }
+
+    /**
+     * 头部添加元素
+     *
+     * @param element
+     */
+    public void addFirst(int element) {
+        add(0, element);
+    }
+
+    /**
+     * 在指定位置插入元素
+     *
+     * @param index
+     * @param element
+     * @return
+     */
+    public void add(int index, int element) {
+        if (size == data.length) {
+            throw new IllegalArgumentException("添加失败.数组满了");
+        }
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("下标错误");
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = element;
+        size++;
+    }
+
+    /**
+     * 根据下标获取元素
+     *
+     * @param index
+     * @return
+     */
+    public int get(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("下标错误");
+        }
+        return data[index];
+    }
+
+    /**
+     * 指定下标设置元素
+     *
+     * @param index
+     * @param element
+     */
+    public void set(int index, int element) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("下标错误");
+        }
+        data[index] = element;
+    }
+
+    /**
+     * 是否包含指定元素
+     *
+     * @param element
+     * @return
+     */
+    public boolean contains(int element) {
+        return find(element) >= 0 ? true : false;
+    }
+
+    /**
+     * 查找指定元素的索引
+     *
+     * @param element
+     * @return
+     */
+    public int find(int element) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 删除指定位置的元素
+     * @param index
+     * @return
+     */
+    public int remove(int index){
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("下标错误");
+        }
+        int ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i + 1] = data[i];
+        }
+        size--;
+        return ret;
+    }
+
+    /**
+     * 删除首位元素
+     * @return
+     */
+    public int removeFirst(){
+        return remove(0);
+    }
+
+    /**
+     * 删除末尾元素
+     * @return
+     */
+    public int removeLast(){
+        return remove(size - 1);
+    }
+
+    /**
+     * 删除一个指定元素
+     * @param element
+     */
+    public void removeElement(int element){
+        int index = find(element);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    /**
+     * 删除所有的指定元素
+     * @param element
+     */
+    public void removeAllElement(int element){
+        //todo
+        int index = find(element);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    /**
+     * 查找所有的指定元素下标
+     * @return
+     */
+    public int[] findAll(int element){
+        //todo
+        return new int[]{};
+    }
+
+    @Override
+    public String toString() {
+        return "Array{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
+    }
 }
--- array/src/Main.java
+++ array/src/Main.java
@@ -8,19 +8,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = new int[20];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
+        Array arr = new Array(20);
+        for (int i = 0; i < 10; i++) {
+            arr.addLast(i);
         }
+        System.out.println(arr);
 
-        int[] scores = new int[]{100, 200, 333};
-        for (int i = 0; i < scores.length; i++) {
-            System.out.println(scores[i]);
-        }
+        arr.add(1, 100);
+        System.out.println(arr);
 
-        for (int score : scores) {
-            System.out.println(score);
-        }
+        arr.addFirst(-1);
+        System.out.println(arr);
     }
 }
 
